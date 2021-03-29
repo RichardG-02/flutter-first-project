3f26955be925130c0d83e2a1616b6eb0523870ca
RichardG-02 <rg02.github@gmail.com>
2021-03-29 20:10:49 +0200

    Adding a button

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -20,48 +20,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Hello World")), body: TextInputWidget());
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class TextInputWidget extends StatefulWidget {
-  @override
-  _TextInputWidgetState createState() => _TextInputWidgetState();
-}
-
-class _TextInputWidgetState extends State<TextInputWidget> {
-  final controller = TextEditingController();
+class _MyHomePageState extends State<MyHomePage> {
   String text = "";
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
-  }
 
-  void changeText(text) {
-    if (text == "Hello World!") {
-      controller.clear();
-      text = "";
-    }
-    setState(() {
+  void changeText(String text) {
+    this.setState(() {
       this.text = text;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      TextField(
+    return Scaffold(
+        appBar: AppBar(title: Text("Hello World")),
+        body: Column(children: <Widget>[
+          TextInputWidget(this.changeText),
+          Text(this.text)
+        ]));
+  }
+}
+
+class TextInputWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  TextInputWidget(this.callback);
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void click() {
+    widget.callback(controller.text);
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
         controller: this.controller,
         decoration: InputDecoration(
-            prefixIcon: Icon(Icons.message), labelText: "Type a message: "),
-        onChanged: (text) => this.changeText(text),
-      ),
-      Text(this.text)
-    ]);
+            prefixIcon: Icon(Icons.message),
+            labelText: "Type a message:",
+            suffixIcon: IconButton(
+              icon: Icon(Icons.send),
+              splashColor: Colors.blue,
+              tooltip: "Post message",
+              onPressed: this.click,
+            )));
   }
 }
